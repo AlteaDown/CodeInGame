15366166ecf2b1945b1e2be34507d4ee177e8832
AlteaDown <polypanel01>
2016-09-21 21:24:48 -0600

    Completed solution for Defibrillator.

--- src/com/company/Defibrilator/Solution.java
+++ src/com/company/Defibrilator/Solution.java
@@ -1,4 +1,4 @@
-
+package Defibrilator;
 
 import java.util.Scanner;
 
@@ -10,29 +10,25 @@ public class Solution {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        double longitudeB = Math.toRadians(Double.parseDouble(in.nextLine().replace(',', '.')));
-        double latitudeB = Math.toRadians(Double.parseDouble(in.nextLine().replace(',', '.')));
+        double latitudeB = Double.parseDouble(in.nextLine().replace(',', '.'));
+        double longitudeB = Double.parseDouble(in.nextLine().replace(',', '.'));
         int N = Integer.parseInt(in.nextLine());
         //in.nextLine();
         String nearestDefibrillator = null;
         double nearestDistance = -1;
         for (int i = 0; i < N; i++) {
             String[] defib = in.nextLine().split(";");
-            double latitudeA = Math.toRadians(Double.parseDouble(defib[4].replace(',', '.')));
-            double longitudeA = Math.toRadians(Double.parseDouble(defib[5].replace(',', '.')));
+            double latitudeA = Double.parseDouble(defib[4].replace(',', '.'));
+            double longitudeA = Double.parseDouble(defib[5].replace(',', '.'));
 
-            double x = (longitudeB - longitudeA) * Math.cos((latitudeA + latitudeB) / 2);
-            double y = latitudeB - latitudeA;
-            double distance = Math.sqrt(x*x + y*y) * 6371;
+            double x = Math.toRadians(longitudeB - longitudeA) * Math.cos(Math.toRadians(latitudeA + latitudeB) / 2.0);
+            double y = Math.toRadians(latitudeB - latitudeA);
+            double distance = Math.sqrt(x * x + y * y) * 6371.0;
             if (nearestDefibrillator == null || distance < nearestDistance) {
                 nearestDefibrillator = defib[1];
                 nearestDistance = distance;
             }
         }
-
-
-        // Write an action using System.out.println()
-        // To debug: System.err.println("Debug messages...");
 
         System.out.println(nearestDefibrillator);
     }
